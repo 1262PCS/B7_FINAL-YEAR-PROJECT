da82336c4155403729ab54dc9eb6991fee61e67e
Janvi P <pj387587@gmail.com>
2024-02-21 00:15:37 +0530

    UI

--- ui/app.py
+++ ui/app.py
@@ -5,7 +5,6 @@ from db import create_table, add_user, authenticate_user
 
 app = Flask(__name__)
 
-# Dummy database for users (replace this with a real database in production)
 create_table()
 
 @app.route('/')
@@ -17,11 +16,10 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['psw']
-        # Check if username and password match in the database
+        
         if authenticate_user(email, password): 
-                # Redirect to home page upon successful login
             return redirect(url_for('home'))
-        # If credentials don't match, show login page again
+        
         return render_template('login.html', message='Invalid username or password')
     return render_template('login.html')
 
@@ -33,17 +31,13 @@ def signup():
         password = request.form['psw']
         confirm_password = request.form['confirm']
 
-        # Check if passwords match
         if password != confirm_password:
             return render_template('signup.html', message='Passwords do not match')
 
         try:
-            # Attempt to add the user to the database
             add_user(username, email, password)
-            # Redirect to login page after successful signup
             return redirect(url_for('login'))
         except ValueError as e:
-            # Handle the ValueError raised by add_user
             return render_template('signup.html', message=str(e))
 
     return render_template('signup.html')
